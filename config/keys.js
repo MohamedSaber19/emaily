0c0e86a020869e80fc6574b5c24dc51aadeca67d
Mohamed Saber <mohamed.saber@itworx.com>
2022-08-15 15:38:33 +0200

    revert back env check

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,6 @@
 // check which credentials and keys to return based on env [prod,dev]
-// if (process.env.NODE_ENV === "production") {
-//   module.exports = require("./prod");
-// } else {
-//   module.exports = require("./dev");
-// }
-
-module.exports = require("./prod");
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./prod");
+} else {
+  module.exports = require("./dev");
+}
