adfba8b6030b6062bd5f7190802b66f40edf5aef
Mohamed Saber <mohamed.saber@itworx.com>
2022-08-15 15:04:18 +0200

    test prod keys

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,8 @@
 // check which credentials and keys to return based on env [prod,dev]
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./prod");
-} else {
-  module.exports = require("./dev");
-}
+// if (process.env.NODE_ENV === "production") {
+//   module.exports = require("./prod");
+// } else {
+//   module.exports = require("./dev");
+// }
+
+module.exports = require("./prod");
